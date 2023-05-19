bd772035f4c45c645809327f347030ce2619ea17
sydnief <sydniefarrell@gmail.com>
2023-05-19 14:34:21 -0400

    updated files/began setting up models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,12 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(ProductTag, {
+  foreignKey: 'productTag_id',
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  foreignKey: 'productTag_id',
+})
 
 module.exports = {
   Product,
