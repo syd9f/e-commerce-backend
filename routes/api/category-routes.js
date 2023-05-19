744e3cbfe39ac5b09593d311363296ab42564deb
sydnief <sydniefarrell@gmail.com>
2023-05-19 14:43:08 -0400

    updated category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,14 +2,36 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
+router.use('/api/categories', Category)
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Category }, {model: Product}],
+    })
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Category }, {model: Product}],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'Category not found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
