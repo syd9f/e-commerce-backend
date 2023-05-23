0ae28ffa10450d52217142874703ad26a701897a
sydnief <sydniefarrell@gmail.com>
2023-05-22 20:47:47 -0400

    fixed all columns for models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,28 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    productTag_id: {
+      type: dataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: dataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'product_id',
+        unique: false
+      }
+    },
+    tag_id: {
+      type: dataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'tag_id',
+        unique: false
+      }
+    }
   },
   {
     sequelize,
