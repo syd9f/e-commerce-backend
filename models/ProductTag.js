bd8e1c405393e5783ce0d66081d6d66a1efa1fbf
sydnief <sydniefarrell@gmail.com>
2023-06-22 19:10:28 -0400

    fixed typos

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,13 +7,13 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     productTag_id: {
-      type: dataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
     product_id: {
-      type: dataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       references: {
         model: 'product',
         key: 'product_id',
@@ -21,7 +21,7 @@ ProductTag.init(
       }
     },
     tag_id: {
-      type: dataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       references: {
         model: 'tag',
         key: 'tag_id',
